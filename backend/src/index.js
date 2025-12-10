6e5ca69f9e5d69b6eb8f3da5f430e669614e6a9b
milopez-tech <milopez@fi.uba.ar>
2025-12-10 13:05:10 -0300

    Agrego la base del backend, todavia no funciona en la pag

---
+++ backend/src/index.js
@@ -0,0 +1,13 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Backend funcionando" });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor corriendo en http://localhost:" + PORT);
+});
