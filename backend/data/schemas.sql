40d8fa536c7b13169e658838891729db85b140de
milopez-tech <milopez@fi.uba.ar>
2025-12-12 12:40:53 -0300

    agrego la endpoint para poder agregar usuarios

--- backend/data/schemas.sql
+++ backend/data/schemas.sql
@@ -1,3 +1,32 @@
 CREATE TABLE usuarios (id SERIAL PRIMARY KEY, nombre VARCHAR(15) NOT NULL, apellido VARCHAR(15) NOT NULL, edad INTEGER NOT NULL, usuario VARCHAR(15) NOT NULL, contrasena VARCHAR(15) NOT NULL);
 CREATE TABLE recetas (id SERIAL PRIMARY KEY, id_usuarios INTEGER, nombre TEXT NOT NULL, descripcion TEXT NOT NULL, tiempo_preparacion INTEGER NOT NULL, categoria VARCHAR(20) NOT NULL, likes INTEGER, dislikes INTEGER, FOREIGN KEY (id_usuarios) REFERENCES usuarios(id));
 CREATE TABLE comentarios (id SERIAL PRIMARY KEY, id_usuarios INTEGER, id_recetas INTEGER, descripcion TEXT NOT NULL, likes INTEGER, dislikes INTEGER, FOREIGN KEY (id_usuarios) REFERENCES usuarios(id), FOREIGN KEY (id_recetas) REFERENCES recetas(id));
+
+-insertar clientes de prueba-
+
+INSERT INTO usuarios (id, nombre, apellido, edad, usuario, contrasena) VALUES ("ricardo", "rodrigues", 23, "tini", "nose");
+
+-comando para enviar una peticion HTTP POST desde la terminal-
+(se envia un JSON como un body parra poder probar el post)
+
+//1- pruebo de forma ideal (pasa)
+curl -X POST \
+-d '{"nombre":"matias", "apellido":"zapata", "edad":23, "usuario":"sergio44", "contrasena":"todomas"}'\
+-H "Content-Type: application/json" \
+http://localhost:3000/usuarios
+
+//2- pruebo errores del post en usuarios, dejo un espacio en blanco en apellido (marca error)
+curl -X POST \
+-d '{"nombre":"aaaaa", "apellido":"", "edad":20, "usuario":"gsgwg", "contrasena":"elmascapo"}' \
+-H "Content-Type: application/json" \
+http://localhost:3000/usuarios
+
+
+//3- pruebo errores del post en usuarios, no pongo el apellido (marca error)
+curl -X POST \
+-d '{"nombre":"aaaaa", "edad":20, "usuario":"gsgwg", "contrasena":"elmascapo"}' \
+-H "Content-Type: application/json" \
+http://localhost:3000/usuarios
+
+
+
--- backend/src/index.js
+++ backend/src/index.js
@@ -4,22 +4,56 @@ import { pool } from "./db.js";
 
 const app = express();
 
-app.use(cors()); 
 app.use(express.json());
+app.use(cors()); 
 
 app.get("/", (req, res) => {
   res.json({ message: "Backend funcionando" });
 });
 
-app.get("/users", async (req, res) => {
+// GET. /usuarios
+app.get("/usuarios", async (req, res) => {
   try {
-    const result = await pool.query("SELECT * FROM users");
+    const result = await pool.query("SELECT * FROM usuarios");
     res.json(result.rows);
   } catch (err) {
     console.error(err);
     res.status(500).json({ error: "DB error" });
   }
 });
+
+// POST. /usuarios
+app.post("/usuarios", async (req, res) => {
+  //guarda los valores
+  const nombre = req.body.nombre;
+  const apellido = req.body.apellido;
+  const edad = req.body.edad;
+  const usuario = req.body.usuario;
+  const contrasena = req.body.contrasena;
+
+  //trim() sirve para que no te permita dejar espacios en blanco
+  try {
+    if (!nombre.trim() || !apellido.trim() || !edad || !usuario.trim() || !contrasena.trim()) {
+      return res.status(400).json({ error: 'Faltan campos obligatorios o están vacíos' });
+    }
+    
+    if (edad <= 0) {
+      return res.status(406).json({ error: 'Numero de edad invalido' });
+    }
+
+    const query = `insert into usuarios (nombre, apellido, edad, usuario, contrasena) 
+    values ('${nombre}', '${apellido}', '${edad}', '${usuario}', '${contrasena}')`;
+
+    await pool.query(query);
+    res.json();
+    res.status(200).json({ message: 'Usuario creado correctamente' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error en el servidor' });
+  }
+
+});
+
+
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
