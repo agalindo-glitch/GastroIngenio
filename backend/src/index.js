fa24b42f8c77bcf818306219b9372d67752aede1
milopez-tech <milopez@fi.uba.ar>
2025-12-13 18:00:42 -0300

    Agrego las funciones de los endpoints para la tabla recetas

--- backend/src/index.js
+++ backend/src/index.js
@@ -125,3 +125,117 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log("Servidor corriendo en http://localhost:" + PORT);
 });
+
+// GET. /recetas (busco todos los recetas de la tabla)
+app.get("/recetas", async (req, res) => {
+  try {
+    const result = await pool.query("SELECT * FROM recetas");
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "DB error" });
+  }
+});
+
+// GET. /recetas/<id> (busco un usuario por su id)
+app.get("/recetas/:id", async (req, res) => {
+  try {
+    const id = req.params.id;
+    const result = await pool.query(`SELECT * FROM recetas where id = ${id}`);
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "DB error" });
+  }
+});
+
+// POST. /recetas (creo un receta)
+app.post("/recetas", async (req, res) => {
+  try {
+    //guarda los valores
+    const id_usuario = req.body.id_usuario;
+    const nombre = req.body.nombre;
+    const descripcion = req.body.descripcion;
+    const tiempo_preparacion = req.body.tiempo_preparacion;
+    const categoria = req.body.categoria;
+    const likes = req.body.likes;
+    const dislikes = req.body.dislikes;
+
+
+    //trim() sirve para que no te permita dejar espacios en blanco
+    if (!id_usuario || !nombre.trim() || !descripcion.trim() || !tiempo_preparacion || !categoria.trim()) {
+      return res.status(400).json({ error: 'Faltan campos obligatorios o están vacíos' });
+    }
+
+    if (nombre.length > 30 || categoria.length > 15) {
+      return res.status(400).json({ error: 'Un campo tiene más de 15 caracteres' });
+    }
+
+    if (tiempo_preparacion <= 0 || likes < 0 || dislikes < 0) {
+      return res.status(406).json({ error: 'Numero de alguno de los campos es invalido' });
+    }
+
+    const query = `insert into recetas (id_usuario, nombre, descripcion, tiempo_preparacion, categoria, likes, dislikes) 
+    values ('${id_usuario}','${nombre}', '${descripcion}', '${tiempo_preparacion}', '${categoria}', '${likes}', '${dislikes}')`;
+
+    await pool.query(query);
+    res.status(200).json({ message: 'Receta creada correctamente' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error en el servidor' });
+  }
+
+});
+
+// PUT. /recetas/<id> (modifico una receta por id)
+app.put("/recetas/:id", async (req, res) => {
+  try {
+    //guarda los valores
+    const id = req.params.id;
+    const id_usuario = req.body.id_usuario;
+    const nombre = req.body.nombre;
+    const descripcion = req.body.descripcion;
+    const tiempo_preparacion = req.body.tiempo_preparacion;
+    const categoria = req.body.categoria;
+    const likes = req.body.likes;
+    const dislikes = req.body.dislikes;
+
+    //trim() sirve para que no te permita dejar espacios en blanco
+    if (!id_usuario || !nombre.trim() || !descripcion.trim() || !tiempo_preparacion || !categoria.trim()) {
+      return res.status(400).json({ error: 'Faltan campos obligatorios o están vacíos' });
+    }
+
+    if (nombre.length > 30 || categoria.length > 15) {
+      return res.status(400).json({ error: 'Un campo tiene más de 15 caracteres' });
+    }
+
+    if (tiempo_preparacion <= 0 || likes < 0 || dislikes < 0) {
+      return res.status(406).json({ error: 'Numero de alguno de los campos es invalido' });
+    }
+
+    const query = `update recetas
+    set id_usuario = '${id_usuario}', nombre = '${nombre}', descripcion = '${descripcion}', tiempo_preparacion = '${tiempo_preparacion}', categoria = '${categoria}', likes = '${likes}', dislikes = '${dislikes}'
+    where id = '${id}'`;
+    
+    await pool.query(query)
+    res.status(204).send();
+  } catch (error) {
+    res.status(500).json({ error: 'Error en el servidor' });
+  }
+
+});
+
+// DELETE. /recetas (elimino un usuario por su id, pero no como parametro)
+app.delete("/recetas", async (req, res) => {
+
+  try {
+    const id = req.body.id;
+    const query = `delete from recetas where id = '${id}'`;
+
+    await pool.query(query);
+    res.json(`la receta con el id: '${id}' fue eliminado`);
+    res.status(204).json({ message: 'Receta eliminada correctamente' });
+  } catch (error) {
+    res.status(500).json({ error: 'Error en el servidor' });
+  }
+
+});
