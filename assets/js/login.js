ab7ef082ad858e2d901e86b96939a9f0746602cb
milopez-tech <milopez@fi.uba.ar>
2025-12-15 19:26:49 -0300

    Agrego eventos en los botones para loguearse

---
+++ assets/js/login.js
@@ -0,0 +1,47 @@
+
+function login(){
+    fetch("http://localhost:3000/login",{
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            usuario: usuario.value,
+            contrasena: contrasena.value
+        })
+    })
+    .then(res => {
+        if(!res.ok) throw new Error();
+        return res.json();
+    })
+    .then(user => {
+        localStorage.setItem("logueado", "true");
+        localStorage.setItem("id_usuario", user.id);
+        window.location.href = "index.html"
+    })
+    .catch(() => alert("Los datos ingresados no son correctos"))
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+  setTimeout(function() {
+    const deslogearse_boton = document.getElementById("deslogearse_boton");
+    const perfil_boton = document.getElementById("perfil_boton");
+    const logearse_boton = document.getElementById("logearse_boton");
+
+    if (localStorage.getItem("logeado") == "true") {
+      logearse_boton.style.display = "none";
+      deslogearse_boton.style.display = "block";
+      perfil_boton.style.display = "block";
+    } else {
+      deslogearse_boton.style.display = "none";
+      perfil_boton.style.display = "none";
+      logearse_boton.style.display = "block";
+    }
+
+    deslogearse_boton.onclick = () => {
+      localStorage.removeItem("logeado");
+      location.reload();
+    };
+  }, 100);
+});
