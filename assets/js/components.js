62d539787de14c5ed6079aa96b97627fbc8ebec9
TupacGodoy <stgodoy@fi.uba.ar>
2026-01-21 17:02:01 -0300

    Integración de la barra de búsqueda en components.js

--- assets/js/components.js
+++ assets/js/components.js
@@ -12,53 +12,80 @@ function loadComponent(id, file) {
         .catch(err => console.error(err));
 }
 
-async function initializeHeader() {
-    const crearRecetaBtn = document.getElementById("crearReceta_boton");
-    const perfilBtn = document.getElementById("perfil_boton");
-    const perfilImg = document.getElementById("perfil_img");
-    const loginBtn = document.getElementById("loguearse_boton");
+function initializeHeader () {
+    
+    async function initializeButtons(inputDesktop, btnDesktop,) {
+        
+        const crearRecetaBtn = document.getElementById("crearReceta_boton");
+        const perfilBtn = document.getElementById("perfil_boton");
+        const perfilImg = document.getElementById("perfil_img");
+        const loginBtn = document.getElementById("loguearse_boton");
+    
+        const logueado = localStorage.getItem("logueado") === "true";
+        const id_usuario = localStorage.getItem("id_usuario");
+    
+        // USUARIO NO LOGUEADO
+        if (!logueado) {
+          crearRecetaBtn.style.display = "none";
+          perfilBtn.style.display = "none"  
+          return;
+        }
+    
+        // USUARIO LOGUEADO
+        crearRecetaBtn.style.display = "block";
+        perfilBtn.style.display = "flex";
+        loginBtn.style.display = "none";
+    
+        // CARGAR FOTO PERFIL
+        try {
+          const res = await fetch(`http://localhost:3000/usuarios/${id_usuario}`);
+          const user = await res.json();
+    
+          if (user.foto_perfil?.trim()) {
+            perfilImg.src = user.foto_perfil;
+          }
+          else { 
+            perfilImg.src = "../assets/img/default-user.png";
+          }
+    
+        } catch (error) {
+          console.error("Error al cargar la foto de perfil", error);
+        }
+    }   
 
-    const logueado = localStorage.getItem("logueado") === "true";
-    const id_usuario = localStorage.getItem("id_usuario");
+    function initializeSearchBar () {
+        const inputDesktop = document.querySelector(".navbar__search-input:not(#search-input)");
+        const btnDesktop = document.querySelector(".navbar__search-control .navbar__search-button");
 
-    // USUARIO NO LOGUEADO
-    if (!logueado) {
-      crearRecetaBtn.style.display = "none";
-      perfilBtn.style.display = "none"  
-      return;
+        function ejecutarBusqueda(valor) {
+
+            if (!valor.trim()) {
+                console.warn("⚠ No se puede buscar un texto vacío");
+                return;
+            }
+
+            window.location.href = `/pages/resultados.html?query=${encodeURIComponent(valor)}`;
+        }
+
+        // -------- DESKTOP --------
+        if (inputDesktop) {
+            inputDesktop.addEventListener("keydown", (e) => {
+                if (e.key === "Enter") {
+                    ejecutarBusqueda(inputDesktop.value);
+                }
+            });
+        }
+
+        if (btnDesktop) {
+            btnDesktop.addEventListener("click", () => {
+                ejecutarBusqueda(inputDesktop.value);
+            });
+        }
     }
 
-    // USUARIO LOGUEADO
-    crearRecetaBtn.style.display = "block";
-    perfilBtn.style.display = "flex";
-    loginBtn.style.display = "none";
-
-    // CARGAR FOTO PERFIL
-    try {
-      const res = await fetch(`http://localhost:3000/usuarios/${id_usuario}`);
-      const user = await res.json();
-
-      if (user.foto_perfil?.trim()) {
-        perfilImg.src = user.foto_perfil;
-      }
-      else { 
-        perfilImg.src = "../assets/img/default-user.png";
-      }
-
-    } catch (error) {
-      console.error("Error al cargar la foto de perfil", error);
-    }
-
+    initializeButtons();
+    initializeSearchBar();
 }
-
-// No está en uso
-// window.addEventListener("scroll", () => {
-//     if (window.scrollY > 80) {
-//         navbarMenu.classList.add("scrolled");
-//     } else {
-//         navbarMenu.classList.remove("scrolled");
-//     }
-// });   
 
 
 document.addEventListener("DOMContentLoaded", async() => {
