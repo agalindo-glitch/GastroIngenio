46db4b40833b6f5046a44d9010f8209a4b9d43ae
TupacGodoy <stgodoy@fi.uba.ar>
2026-01-21 16:05:59 -0300

    arreglo bug imagen default. modularización de la inicialización del header en js

--- assets/js/components.js
+++ assets/js/components.js
@@ -1,4 +1,4 @@
-"use strict"
+"use strict";
 
 function loadComponent(id, file) {
     return fetch(`../../components/${file}`)
@@ -10,6 +10,45 @@ function loadComponent(id, file) {
             document.getElementById(id).innerHTML = html;
         })
         .catch(err => console.error(err));
+}
+
+async function initializeHeader() {
+    const crearRecetaBtn = document.getElementById("crearReceta_boton");
+    const perfilBtn = document.getElementById("perfil_boton");
+    const perfilImg = document.getElementById("perfil_img");
+    const loginBtn = document.getElementById("loguearse_boton");
+
+    const logueado = localStorage.getItem("logueado") === "true";
+    const id_usuario = localStorage.getItem("id_usuario");
+
+    // USUARIO NO LOGUEADO
+    if (!logueado) {
+      crearRecetaBtn.style.display = "none";
+      perfilBtn.style.display = "none"  
+      return;
+    }
+
+    // USUARIO LOGUEADO
+    crearRecetaBtn.style.display = "block";
+    perfilBtn.style.display = "flex";
+    loginBtn.style.display = "none";
+
+    // CARGAR FOTO PERFIL
+    try {
+      const res = await fetch(`http://localhost:3000/usuarios/${id_usuario}`);
+      const user = await res.json();
+
+      if (user.foto_perfil?.trim()) {
+        perfilImg.src = user.foto_perfil;
+      }
+      else { 
+        perfilImg.src = "../assets/img/default-user.png";
+      }
+
+    } catch (error) {
+      console.error("Error al cargar la foto de perfil", error);
+    }
+
 }
 
 // No está en uso
@@ -26,58 +65,6 @@ document.addEventListener("DOMContentLoaded", async() => {
     
     loadComponent("footer", "footer.html");
  
-
-    // === HEADER ===
-    await loadComponent("header", "header.html");; 
-
-    const crearRecetaBtn = document.getElementById("crearReceta_boton");
-    const perfilBtn = document.getElementById("perfil_boton");
-    const perfilImg = document.getElementById("perfil_img");
-    const loginBtn = document.getElementById("loguearse_boton");
-
-    const logueado = localStorage.getItem("logueado") === "true";
-    const id_usuario = localStorage.getItem("id_usuario");
-
-    // ====== USUARIO NO LOGUEADO ======
-    if (!logueado) {
-      crearRecetaBtn.style.display = "none";
-
-      return;
-    }
-
-    // ====== USUARIO LOGUEADO ======
-    crearRecetaBtn.style.display = "block";
-    perfilBtn.style.display = "flex";
-    loginBtn.style.display = "none";
-
-    // ====== CARGAR FOTO PERFIL ======
-    try {
-      const res = await fetch(`http://localhost:3000/usuarios/${id_usuario}`);
-      const user = await res.json();
-
-      if (user.foto_perfil?.trim()) {
-        perfilImg.src = user.foto_perfil;
-      }
-
-      perfilImg.onerror = () => {
-        // perfilImg.src = "../assets/img/default-user.png";
-      };
-
-
-      // Si la imagen falla
-      perfilImg.onerror = () => {
-        // perfilImg.src = "../assets/img/default-user.png";
-      };
-
-    } catch (error) {
-      console.error("Error al cargar la foto de perfil", error);
-      // perfilImg.src = "../assets/img/default-user.png";
-    }
-
-    // ====== CLICK EN FOTO → PERFIL ======
-    perfilBtn.addEventListener("click", () => {
-      window.location.href = "../pages/usuario.html";
-    });
-
-
+    await loadComponent("header", "header.html");
+    initializeHeader();
 });
